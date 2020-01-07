60bfa2bb846f1d2c2620afc8f600f91be0af7c37
Efaliso <efalisotamerat@gmail.com>
2020-01-07 21:17:23 +0100

    v2 added

---
+++ ContainerShipment/ContainerShipmentV2/Container.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContainerShipmentV2
+{
+    public class Container
+    {
+        public int Weight { get; set; }
+        public int WeightAbove { get; set; }
+        public ContainerType ContainerType { get; }
+
+        public Container(int weight, ContainerType containerType)
+        {
+            Weight = weight;
+            WeightAbove = 0;
+            ContainerType = containerType;
+        }
+
+        public void AddWeightAbove(int weight)
+        {
+            this.WeightAbove += weight;
+        }
+
+        public string ToString(int a)
+        {
+            if (a == 1)
+                switch (ContainerType)
+                {
+                    case ContainerType.Cooled:
+                        return $"[C] {this.Weight}";
+                    case ContainerType.Normal:
+                        return $"[N] {this.Weight}";
+                    case ContainerType.Valuable:
+                        return $"[V] {this.Weight}";
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            return a == 2 ? $"{this.Weight}" : $"{this.WeightAbove}";
+        }
+    }
+
+    public enum ContainerType
+    {
+        Cooled,
+        Normal,
+        Valuable
+    }
+}
---
+++ ContainerShipment/ContainerShipmentV2/Stack.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace ContainerShipmentV2
+{
+    public class Stack
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public StackPosition StackPosition { get; set; }
+        public List<Container> Containers { get; set; }
+
+        public Stack(int x, int y, StackPosition stackPosition)
+        {
+            X = x;
+            Y = y;
+            StackPosition = stackPosition;
+            Containers = new List<Container>();
+        }
+
+        public bool WeightExceeded(int weight) => Containers.Where(c => Containers.IndexOf(c) != 0).Sum(c => c.Weight) + weight > 120;
+
+        public bool ContainerCanBeAdded(Container container)
+        {
+            if (container.ContainerType == ContainerType.Valuable && this.StackPosition == StackPosition.Back || this.StackPosition == StackPosition.Front) return false;
+            if (container.ContainerType == ContainerType.Cooled && this.StackPosition != StackPosition.Front) return false;
+        }
+
+        public void AddContainer(Container container)
+        {
+            // TODO: Checks aren't done yet 
+            foreach (var c in Containers)
+            {
+                c.AddWeightAbove(container.Weight);
+            }
+            Containers.Add(container);
+        }
+    }
+
+    public enum StackPosition
+    {
+        Front,
+        Back,
+        Center
+    }
+}
